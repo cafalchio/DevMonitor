38c050f44124e00839a76a94cc0fcc5f4fcf2bd7
cafalchio <maolivei@tcd.ie>
2025-10-11 10:15:03 +0100

    release new test version

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -32,7 +32,7 @@ export default function SettingsDrawer() {
 
                     <div className="fixed inset-0 overflow-hidden">
                         <div className="absolute inset-0 overflow-hidden">
-                            <div className="pointer-events-none fixed inset-y-0 left-0 flex max-w-full">
+                            <div className="pointer-events-none fixed inset-y-0 left-0 flex w-screen max-w-none">
                                 <TransitionChild
                                     as={Fragment}
                                     enter="transform transition ease-in-out duration-300"
@@ -42,7 +42,8 @@ export default function SettingsDrawer() {
                                     leaveFrom="translate-x-0"
                                     leaveTo="-translate-x-full"
                                 >
-                                    <DialogPanel className="pointer-events-auto flex h-full w-5/6 max-w-md flex-col bg-white shadow-xl">
+                                    <DialogPanel className="pointer-events-auto flex h-full w-[70vw] max-w-3xl flex-col bg-white shadow-xl">
+                                        {" "}
                                         {/* Header */}
                                         <div className="flex items-center justify-end px-3 pt-[calc(env(safe-area-inset-top)+.8rem)]">
                                             <button
@@ -52,7 +53,6 @@ export default function SettingsDrawer() {
                                                 <XMarkIcon className="h-6 w-6" />
                                             </button>
                                         </div>
-
                                         {/* Panel content */}
                                         <div className="overflow-y-auto p-4">
                                             <AddPCForm
